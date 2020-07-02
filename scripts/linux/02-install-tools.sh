0b2ec7694f980a86e73a579daa11f0108270b7a1
dlcf <15130091299@139.com>
2020-07-02 15:15:19 +0800

    Update 02-install-tools.sh

--- scripts/linux/02-install-tools.sh
+++ scripts/linux/02-install-tools.sh
@@ -8,7 +8,7 @@ green='\033[0;32m'
 yellow='\033[0;33m'
 plain='\033[0m'
 
-toolsName="lsof lrzsz telnet traceroute net-tools bash-completion vim yum-utils yum-plugin-versionlock python3-pip git sshpass mtr conntrack-tools libseccomp libtool-ltdl device-mapper-persistent-data lvm2 conntrack ipvsadm ipset jq sysstat iotop iftop htop tree glances fio"
+toolsName="lsof lrzsz telnet traceroute net-tools bash-completion vim yum-utils yum-plugin-versionlock python3-pip git sshpass mtr conntrack-tools libseccomp libtool-ltdl device-mapper-persistent-data lvm2 conntrack ipvsadm ipset jq sysstat iotop iftop htop tree glances fio tcping"
 
 function deal(){
   yum install $toolsName -y > /dev/null 2>&1
